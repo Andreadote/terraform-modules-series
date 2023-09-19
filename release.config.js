5c6c044e12db4e6ac5382570ccd438ea5aed0a34
andreadote <andre.mj@yahoo.com>
2023-09-19 17:49:49 -0600

    fix: semantic release
    Contributing-to: https://app.slack.com/client/T05RL64LKST/C05S0NDGHD0 - CLD-3>
    
    Problem
    Solution
    Note

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/Andreadote/terraform-modules-series.git",
+    plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/git',
+    '@semantic-release/github']
+    }
