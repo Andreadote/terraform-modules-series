1bf2e2999ae1b4965700e917f7fc38a9a59bcf2c
andreadote <andre.mj@yahoo.com>
2023-09-20 09:16:52 -0600

    fix: module

--- module_call/main.tf
+++ module_call/main.tf
@@ -1,3 +1,3 @@
 module "tesla-bucket" {
-  source = "git::https://github.com/Andreadote/terraform-modules-series.git//S3_module"
+  source = "git::https://github.com/Andreadote/terraform-modules-series.git//S3_module?ref=v1.2.0"
 }
