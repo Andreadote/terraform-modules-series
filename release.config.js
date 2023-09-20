44e9386b8f5c7adc77f7460e85964a4d1de84636
andreadote <andre.mj@yahoo.com>
2023-09-19 18:18:32 -0600

    2nd

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     branches: "main",
-    repositoryUrl: "https://github.com/Andreadote/terraform-modules-series.git//S3_module",
+    repositoryUrl: "https://github.com/Andreadote/terraform-modules-series",
     plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
