3c5a4bbaf4fb2fca20ac6bf8ac717a876d1be393
andreadote <andre.mj@yahoo.com>
2023-09-29 17:05:03 -0600

    fix: bug removed fix
    Contributing-to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET# CLD-10>
    
    Problem
    Solution
    Note

---
+++ terraform.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+set -x
+
+export TF_LOG="DEBUG"
+export TF_LOG_PATH="./terraform.log"
+
+ENV=dev
+TF_PLAN="${ENV}".tfplan
+
+[ -d .terraform ] && rm -rf .terraform
+rm -f *.tfplan
+
+sleep 5
+
+# Format Terraform files recursively
+terraform fmt -recursive
+
+# Initialize Terraform
+terraform init
+
+# Validate Terraform configuration
+terraform validate
+
+# Generate a Terraform plan and save it to a file
+terraform plan -out=${TF_PLAN}
+
+# Show the Terraform plan in JSON format
+terraform show -json ${TF_PLAN} > ${TF_PLAN}.json
+# Run this command on your local terminal first to install or upgrade Checkov
+############  pip3 install -U checkov    #######################################
+
+# This command will install or upgrade Checkov on your local machine,
+# allowing you to use it for infrastructure as code (IaC) security scanning.
+
+# After the installation, type 'checkov' in your local terminal to see the newest updates.
+
+checkov -f ${TF_PLAN}.json
+
+if [ "$?" -eq "0" ]; then
+     echo "Your configuration is Awesome"
+else
+     echo "Your code needs some work!"
+     exit 200
+fi
+
+if [ ! -f "${TF_PLAN}" ]; then
+     echo "The plan does not exist******** Focus and work harder and smarter"
+     exit 88
+fi
